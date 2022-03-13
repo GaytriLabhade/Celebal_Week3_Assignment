f32b7253c119374a035a941902dfc4a677df9be9
GaytriLabhade <100215874+GaytriLabhade@users.noreply.github.com>
2022-03-13 10:53:21 +0530

    Create stringModuleConstant.py

---
+++ stringModuleConstant.py
@@ -0,0 +1,10 @@
+# Celebal_Week3_Assignment
+#string module constants
+import string
+print(string.ascii_letters)     #letters
+print(string.ascii_lowercase)   #lowercase letters
+print(string.ascii_uppercase)   #uppercase letters
+print(string.digits)            #digits
+print(string.hexdigits)         #hexdigits
+print(string.punctuation)       #punctuation
+print(string.whitespace)        #whitespace
